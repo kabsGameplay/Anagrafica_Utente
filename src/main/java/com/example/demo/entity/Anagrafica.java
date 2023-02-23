e3be76becfce8f4910a9eaab4ca2de739f6da526
Mahinur Islam Kabbo <you@example.com>
2023-02-23 22:47:16 +0100

    ho creato entity di Anagrafica

--- src/main/java/com/example/demo/entity/Anagrafica.java
+++ src/main/java/com/example/demo/entity/Anagrafica.java
@@ -1,6 +1,9 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,15 +11,30 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@Table(name="anagrafica")
+@Table(name = "anagrafica")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Anagrafica {
-Private int id;
-Private String nome;
-Private String cognome;
-Private String telefono;
-Private String email;
-Private String cf;
-Private String indrizzo;
+	@Id
+	@Column(name = "id")
+	@GeneratedValue
+	private int id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "cognome")
+	private String cognome;
+
+	@Column(name = "telefono")
+	private String telefono;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "cf")
+	private String cf;
+
+	@Column(name = "indrizzo")
+	private String indrizzo;
 }
